e5d4058def0c31bc79c992dbf34ab7fb6635a44b
oldmcdonnell <oldmcdonnell@gmail.com>
2024-10-13 16:33:58 -0400

    moving test file to the correct folder, updating HelloController.java

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -2,33 +2,17 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot! verifiy test";
-	}
-	    // Add this method to map to the "/hello" endpoint
-		//
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot! verify test";
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
-
 }
-
