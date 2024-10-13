57b3622119210376d344577b406da1384b3129c7
oldmcdonnell <oldmcdonnell@gmail.com>
2024-10-13 17:06:46 -0400

    test running ok

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! verify test";
+        return "Greetings from Spring Boot!";
     }
 
     @GetMapping("/hello")
